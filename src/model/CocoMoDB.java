e6d5aa9bdb80c8f90c77c6f4e06a59afff8b2314
Oscar Olsson <osse.olsson@gmail.com>
2012-03-23 15:13:03 +0100

    Readable format fixed

--- src/model/CocoMoDB.java
+++ src/model/CocoMoDB.java
@@ -5,13 +5,29 @@ import java.util.HashMap;
 
 public class CocoMoDB implements ProjectDB  {
 	private ArrayList<Project> refProjects;
+
 	private double threshold;
+
 	private double pred;
-	private double[][] values = { { 0.75, 0.88, 1.00, 1.15, 1.40,  0 }, { 0, 0.94, 1.00, 1.08, 1.16, 0 },  { 0.70, 0.85, 1.00, 1.15, 1.30, 1.65 }, 
-			{ 0, 0, 1.00, 1.11, 1.30, 1.66 }, { 0, 0, 1.00, 1.06, 1.21, 1.56 }, { 0, 0.87, 1.00, 1.15, 1.30, 0 }, { 0, 0.87, 1.00, 1.07, 1.15, 0 }, 
-			{ 1.46, 1.19, 1.00, 0.86, 0.71, 0 },  {1.29, 1.13, 1.00, 0.91, 0.82, 0 } , { 1.42, 1.17, 1.00, 0.86, 0.70, 0  }, {1.21, 1.10, 1.00, 0.90, 0, 0 }, 
-			{ 1.14, 1.07, 1.00, 0.95, 0, 0  } , { 1.24, 1.10, 1.00, 0.91, 0.82, 0  } , { 1.24, 1.10, 1.00, 0.91, 0.83, 0 } , {1.23, 1.08, 1.00, 1.04, 1.10, 0 }  }; 
+
 	private HashMap<String,Integer> attributes = new HashMap<String, Integer>(); 
+
+	private double[][] values = {
+		{ 0.75, 0.88, 1.00, 1.15, 1.40, 0   },
+		{ 0,    0.94, 1.00, 1.08, 1.16, 0   },
+		{ 0.70, 0.85, 1.00, 1.15, 1.30, 1.65}, 
+		{ 0,    0,    1.00, 1.11, 1.30, 1.66},
+		{ 0,    0,    1.00, 1.06, 1.21, 1.56},
+		{ 0,    0.87, 1.00, 1.15, 1.30, 0   },
+		{ 0,    0.87, 1.00, 1.07, 1.15, 0   }, 
+		{ 1.46, 1.19, 1.00, 0.86, 0.71, 0   },
+		{ 1.29, 1.13, 1.00, 0.91, 0.82, 0   },
+		{ 1.42, 1.17, 1.00, 0.86, 0.70, 0   },
+		{ 1.21, 1.10, 1.00, 0.90, 0,    0   }, 
+		{ 1.14, 1.07, 1.00, 0.95, 0,    0   },
+		{ 1.24, 1.10, 1.00, 0.91, 0.82, 0   },
+		{ 1.24, 1.10, 1.00, 0.91, 0.83, 0   },
+		{ 1.23, 1.08, 1.00, 1.04, 1.10, 0   } }; 
 
 	
 	public CocoMoDB(){
@@ -22,34 +38,33 @@ public class CocoMoDB implements ProjectDB  {
 		attributes.put("High", 3);
 		attributes.put("Very_High", 4);
 		attributes.put("Extra_High", 5);
-		
-	
 	}
+
 	public void addProject(Project proj){
 		refProjects.add(proj);
 	}
+
 	public ArrayList<Project> getSimilar(Project proj){
 		return refProjects;
 	}
+
 	public void setThreshold(double newThreshold){
 		threshold = newThreshold;
 	}
+
 	private double eucLength(Project source){
-		
 		for(Double d : atrToValue(source.getList())){
 			// do calc lolz
 		}
 		return 0.0;
 	}
+
 	private ArrayList<Double> atrToValue(ArrayList<String> toBeMapped){
 		ArrayList<Double> atr = new ArrayList<Double>();
 		for(int i = 0; i < toBeMapped.size()-2; i++ ){
-			
-		atr.add(values[i][attributes.get(toBeMapped.get(i))]);
+			atr.add(values[i][attributes.get(toBeMapped.get(i))]);
 		}
 		return atr;
-		
 	}
-	 
 
 }
