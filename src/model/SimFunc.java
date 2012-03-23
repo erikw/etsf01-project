707e4afca12a3816965b29d2ec6cc709fa872924
Jonas Klauber <skogs4@hotmail.com>
2012-03-23 11:52:14 +0100

    Added methods to SimFunc.java

--- src/model/SimFunc.java
+++ src/model/SimFunc.java
@@ -3,11 +3,21 @@ package model;
 
 public abstract class SimFunc {
 protected ProjectDB db;
+protected double threshold;
+protected double pred;
 public SimFunc(ProjectDB db){
 	this.db = db;
+	threshold = 0;
+	pred = 0;
 }
 
 public List<Project> simularProj(Project proj);
+public double predProj(){
+	return pred;
+}
 private double compare(Project proj);
+public void setThreshold(double newThreshold){
+	threshold = newThreshold;
+}
 
 }
