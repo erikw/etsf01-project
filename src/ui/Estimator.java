dca68f25af124458fb36afc214da8814cf4ebcae
Erik Westrup <erik.westrup@gmail.com>
2012-05-03 14:27:03 +0200

    TODO added for requirements.

--- src/ui/Estimator.java
+++ src/ui/Estimator.java
@@ -108,7 +108,7 @@ public class Estimator {
 		Project inputProject = new Project(userAttribs);
 
 		Map<Double, Project> similarProjects = db.similarProjects(inputProject);
-		if(similarProjects.size() == 0){
+		if (similarProjects.size() == 0){
 	
 			System.err.println("\u001B[31m\u001B[1mWARNING! No projects within given threshold\u001B[0m");
 		}
